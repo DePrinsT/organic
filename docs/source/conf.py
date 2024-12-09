fc62286a56c1cd507eb99202dd5a08f3f7542a39
Toon De Prins <toon.deprins@protonmail.com>
2024-12-09 20:36:42 +0100

    Changed documentation to use new version of sphinx and sphinx-book-theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -45,7 +45,8 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "renku"
+html_theme = "sphinx_book_theme"
+html_title = "ORGANIC"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -55,6 +56,5 @@ html_static_path = ['_static']
 html_logo = "../logo/organic_logo.png"
 
 html_theme_options = {
-    'logo_only': True,
 }
 
